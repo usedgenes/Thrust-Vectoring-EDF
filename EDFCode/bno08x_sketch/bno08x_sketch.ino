c679a003b21a717dac043e632ccbee79c6007888
usedgenes <67665540+usedgenes@users.noreply.github.com>
2024-08-04 17:30:35 -0700

    Edited Files

---
+++ EDFCode/bno08x_sketch/bno08x_sketch.ino
@@ -0,0 +1,119 @@
+#include "SparkFun_BNO08x_Arduino_Library.h"  
+
+#define BNO08X_CS   33
+#define BNO08X_INT  25
+#define BNO08X_RST  26
+
+SPIClass vspi = SPIClass(VSPI);
+
+void setup() {
+  Serial.begin(115200);
+  vspi.begin(22, 23, 21, 33);
+  while(!Serial) delay(10);
+  Serial.println();
+  Serial.println("BNO08x Read Example");
+
+  if (myIMU.beginSPI(BNO08X_CS, BNO08X_INT, BNO08X_RST, 1000000, vspi) == false) {
+    Serial.println("BNO08x not detected. Check your jumpers and the hookup guide. Freezing...");
+    while (1)
+      ;
+  }
+  Serial.println("BNO08x found!");
+
+  setReports();
+
+  Serial.println("Reading events");
+  delay(100);
+}
+
+void setReports(void) {
+  Serial.println("Setting desired reports");
+  if (myIMU.enableRotationVector() == true) {
+    Serial.println(F("Rotation vector enabled"));
+    Serial.println(F("Output in form i, j, k, real, accuracy"));
+  } else {
+    Serial.println("Could not enable rotation vector");
+  }
+  delay(100); 
+
+  if (myIMU.enableGyro() == true) {
+    Serial.println(F("Gyro enabled"));
+    Serial.println(F("Output in form x, y, z, in radians per second"));
+  } else {
+    Serial.println("Could not enable gyro");
+  }
+  delay(100);
+
+  if (myIMU.enableAccelerometer() == true) {
+    Serial.println(F("Accelerometer enabled"));
+  } else {
+    Serial.println("Could not enable accelerometer");
+  }
+  delay(100);
+}
+
+void loop() {
+  delay(10);
+
+  if (myIMU.wasReset()) {
+    Serial.print("sensor was reset ");
+    setReports();
+  }
+
+  if (myIMU.getSensorEvent() == true) {
+
+    if (myIMU.getSensorEventID() == SENSOR_REPORTID_ROTATION_VECTOR) {
+
+      float quatI = myIMU.getQuatI();
+      float quatJ = myIMU.getQuatJ();
+      float quatK = myIMU.getQuatK();
+      float quatReal = myIMU.getQuatReal();
+      float quatRadianAccuracy = myIMU.getQuatRadianAccuracy();
+      Serial.print("Rotation ");
+      Serial.print(quatI, 2);
+      Serial.print(F(","));
+      Serial.print(quatJ, 2);
+      Serial.print(F(","));
+      Serial.print(quatK, 2);
+      Serial.print(F(","));
+      Serial.print(quatReal, 2);
+      Serial.print(F(","));
+      Serial.print(quatRadianAccuracy, 2);
+
+      Serial.println();
+    }
+
+    if (myIMU.getSensorEventID() == SENSOR_REPORTID_GYROSCOPE_CALIBRATED) {
+      float x = myIMU.getGyroX();
+      float y = myIMU.getGyroY();
+      float z = myIMU.getGyroZ();
+
+      Serial.print("Gyroscope ");
+      Serial.print(x, 2);
+      Serial.print(F(","));
+      Serial.print(y, 2);
+      Serial.print(F(","));
+      Serial.print(z, 2);
+
+      Serial.println();
+    }
+
+    if (myIMU.getSensorEventID() == SENSOR_REPORTID_ACCELEROMETER) {
+
+      float x = myIMU.getAccelX();
+      float y = myIMU.getAccelY();
+      float z = myIMU.getAccelZ();
+      Serial.print("Accelerometer ");
+      Serial.print(x, 2);
+      Serial.print(F(","));
+      Serial.print(y, 2);
+      Serial.print(F(","));
+      Serial.print(z, 2);
+
+      Serial.println();
+    }
+
+    delay(500);
+
+  }
+}
