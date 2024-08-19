db820c273bf2ae4f002f773d40209afdaab4f4cd
usedgenes <67665540+usedgenes@users.noreply.github.com>
2024-08-19 16:03:03 -0700

    Edited Files

--- EDFCode/ControlLoop.cpp
+++ EDFCode/ControlLoop.cpp
@@ -10,12 +10,6 @@ float ControlLoop::ComputeCorrection(float error, float loopTime) {
   float output = (constants.Kp * error) + (constants.Ki * integrator) + (constants.Kd * derivative);
 
   errorPrev = error;
-
-  if (output > MAX_PID_OUTPUT) {
-    output = MAX_PID_OUTPUT;
-  } else if (output < MIN_PID_OUTPUT) {
-    output = MIN_PID_OUTPUT;
-  }
   
   return output;
 }
